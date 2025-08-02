a7b1ffcc4b9f95a547fd45ac4b795225660f0eae
bhumi-rao <151423873+bhumi-rao@users.noreply.github.com>
2025-08-02 17:09:41 +0530

    02.operators.js

---
+++ Basics/02.operators.js
@@ -0,0 +1,32 @@
+// Declaring variables
+let name = "Alien";          // string
+let age = 21;                // number
+let isStudent = true;        // boolean
+let marks = null;            // null
+let address;                 // undefined
+
+// Logging values and types
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is student:", isStudent);
+console.log("Marks:", marks);
+console.log("Address:", address);
+
+// typeof operator
+console.log("Type of name:", typeof name);
+console.log("Type of age:", typeof age);
+console.log("Type of isStudent:", typeof isStudent);
+
+//--------------------------------------------------------------------------------------------------------
+
+const accountId = 12345
+let accountname = "john"
+let accountemail = "john@gmail.com"
+accountcity = "bangalore"
+accountstate = "Karnataka"
+
+//var accountname = "alien" ...[cannot be used bcz of issuse in block scope and functional scope]
+// to display output
+console.log(accountId)
+ // to display output in table form
+console.table([accountId,accountemail,accountname,accountcity,accountstate])
