3b3465aafbba26f4bf672cd687d1ffe696671538
Bhumika D <151423873+bhumi-rao@users.noreply.github.com>
2025-08-05 22:40:01 +0530

    Update 04_objects.js

--- 04_objects.js
+++ 04_objects.js
@@ -1,1 +1,26 @@
+let student = {
+  name: "Kruthika",
+  age: 21,
+  isEnrolled: true
+};
+
+// Accessing Object Properties
+console.log(student.name);      // Dot notation
+console.log(student["age"]);    // Bracket notation
+
+// Adding / Updating / Deleting Properties
+student.grade = "A";         // Add new property
+student.age = 22;            // Update property
+delete student.isEnrolled;  // Remove property
+
+// Object with Methods
+let user = {
+  name: "Kruthika",
+  greet: function() {
+    console.log("Hello, " + this.name);
+  }
+};
+
+user.greet(); // Output: Hello, Kruthika
+
 
