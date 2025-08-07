63d80338d065a513ca04a2c889c078575b0b4c7f
Bhumika D <151423873+bhumi-rao@users.noreply.github.com>
2025-08-07 22:27:59 +0530

    Update 05_conditionalstmts.js

--- 05_conditionalstmts.js
+++ 05_conditionalstmts.js
@@ -1,1 +1,16 @@
+//if , else if, else
+let score = 75;
+
+if (score >= 90) {
+  console.log("Excellent");
+} else if (score >= 60) {
+  console.log("Good");
+} else {
+  console.log("Needs Improvement");
+}
+
+//Ternary Operator
+let age = 18;
+let message = (age >= 18) ? "Adult" : "Minor";
+console.log(message); // Adult
 
